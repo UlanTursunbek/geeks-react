70a6ce39ae6e6484466105d60aa74142916a9e46
UlanTursunbek <ulantursunbekdev@gmail.com>
2023-10-11 16:16:39 +0600

    initial comment

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import './Button.css'
+
+export function Button() {
+    return (
+        <button className='button'> Hi i'm button
+        
+        <div>12312</div>
+        </button>
+    )
+}
